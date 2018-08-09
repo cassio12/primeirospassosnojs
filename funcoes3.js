62bcfc89e65a430f88928261686d8ab0e8a1126e
EstudioVaiNaweb <cassioprazeres241@gmail.com>
2018-08-09 09:56:08 -0300

    progreço nas aulas

--- funcoes3.js
+++ funcoes3.js
@@ -1,8 +1,32 @@
 /*Closure*/
 
-var carros = [];
-carros[0] = 'Ka';
-carros[1] = 'Corsa';
-carros[3] = 'Palio';
+function wrapValue(n) {
+	var localVariable = n;
+	return function() { return localVariable; };
+}
+var warp1 = wrapValue(1);
+var warp2 = wrapValue(2);
+console.log(warp1());
+console.log(warp2());
 
-console.log(carros)
+
+var carros = ['Ka', 'Corsa', 'Palio'];
+
+var pos = carros.indexOf("Corsa");
+carros.splice(0, 2, 'Sonic', 'Uno');
+carros.toString();
+
+console.log(carros);
+
+
+carros.forEach(function(element){
+	console.log(element);
+});
+
+function multiplier(factor) {
+	return function(number) {
+		return number * factor;
+	};
+}
+var twice = multiplier(2);
+console.log(twice(5));
